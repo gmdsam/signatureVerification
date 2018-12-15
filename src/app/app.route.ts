ad561351210f1e00576d8a46681b20ebda093036
Saumil B Hariyani <saumilhariyani35@gmail.com>
2018-12-16 01:58:31 +0530

    Update app.route.ts

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { LoginComponent } from './routes/login/login.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent }
 ];
+
